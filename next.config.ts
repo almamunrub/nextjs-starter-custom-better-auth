d6920872ccd22da09c8917e0d6af706f622e904e
progmamun <mamun333089@gmail.com>
2026-07-23 07:47:30 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -24,21 +24,6 @@ const nextConfig: NextConfig = {
     position: 'bottom-right', // or 'top-right'
   },
   // output: 'standalone', // only for docker if used
-  // better-auth proxy
-  async rewrites() {
-    return [
-      {
-        // Explicitly map auth requests
-        source: '/api/auth/:path*',
-        destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/auth/:path*',
-      },
-      {
-        // Explicitly map v1 API requests
-        source: '/api/v1/:path*',
-        destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/:path*',
-      },
-    ]
-  },
 }
 
 export default nextConfig
